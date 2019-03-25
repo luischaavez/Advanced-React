5b65250470deafd6a6b683e571232ff64cf7757f
Luis Chávez <luischa04@gmail.com>
2019-03-24 21:59:21 -0600

    All changes of module #3

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    items: forwardTo('db')
+    /*dogs(parent, args, context, info) {
+        global.dogs = global.dogs || [];
+
+        return global.dogs;
+    }*/
+};
 
 module.exports = Query;
