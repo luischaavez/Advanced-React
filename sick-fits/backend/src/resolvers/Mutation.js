53837be26a9d4a98001e2933d656757740a0c107
Luis Chávez <luischa04@gmail.com>
2019-03-28 12:51:00 -0600

    All module 4 changes (CRUD of Items)

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -8,14 +8,34 @@ const Mutations = {
         }, info);
 
         return item;
+    },
+    updateItem(parent, args, context, info) {
+        // first take a copy of the updates
+        const updates = { ...args };
+        // remove the ID from the updates
+        delete updates.id;
+        // run the update method
+        return context.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        }, 
+        info
+        );
+    },
+    async deleteItem(parent, args, context, info) {
+        const where = { id: args.id };
+        // 1. Find the item.
+        const item = await context.db.query.item({ where }, `{ 
+            id
+            title
+        }`);
+        // 2. Check if they own that item, or have te permissions.
+        // TODO
+        // 3. Delete it!
+        return context.db.mutation.deleteItem({ where }, info);
     }
-    /*createDog(parents, args, context, info) {
-        global.dogs = global.dogs || [];
-        // create a Dog
-        const newDog = { name: args.name };
-        global.dogs.push(newDog);
-        return newDog;
-    }*/
 };
 
 module.exports = Mutations;
